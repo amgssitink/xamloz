9afda99dff7ce43e894fbf4ef158002e699bf8bf
phoriphaisitink <166979659+amgssitink@users.noreply.github.com>
2024-06-10 21:20:23 +0700

    Add files via upload

--- Testgui.lua
+++ Testgui.lua
@@ -4074,38 +4074,50 @@ if getgenv().Tvk then
 local Main = Library.CreateMain({Title = 'Gay', Desc = ''})
 
 local Page1 = Main:CreatePage("Page 1")
+local Page2 = Main:CreatePage("Page 2")
 
 local Section1 = Page1:CreateSection("Section Name")
+local Section2 = Page2:CreateSection("Name")
+
+Section2:CreateKeybind("Keybind", "T", function(key)
+	print("Pressed "..tostring(key))
+end)
 
 Section1:CreateSlider("Slider Example", {Min = 16, Max = 500, DefaultValue = 30}, function(Value)
-   print(Value)
+	print(Value)
 end)
 
-Section1:CreateToggle("Toggle Example", {Toggled=false , Description = false}, function(Value)
-   print(Value)
+Section1:CreateToggle("Toggle Example", {Toggled = false , Description = "Iu Tvk nhìu UwU"}, function(Value)
+	print(Value)
 end)
 
-Section1:CreateButton("Button Example", function(V)
-   print("Button Cliked!")
+Section1:CreateButton("Button Example", function ()
+	print("Button Cliked!")
 end)
 
-Section1:CreateTextbox("TextBox", false, function(v)
-   print(vv)
+Section1:CreateTextbox("TextBox", false, function (vv)
+	print(vv)
 end)
 
 Section1:CreateDropdown("Dropdown ", {
-   List = {"Value1", "Value2", "Value3", "Value4"},
-   Default = "None"}, function(value)
-       print(value)
+	List = {"Value1", "Value2", "Value3", "Value4"},
+	Default = "None"}, function(value)
+	print(value)
 end)
 
-Section1:CreateColorPicker("Color Picker", Color3.fromRGB(255, 255, 255), function()
-   print("fsf")
+Section1:CreateColorPicker("Color Picker", Color3.fromRGB(255, 255, 255), function (uwu)
+	print(uwu)
 end)
 
-Section1:CreateButton("Refresh Example", function(Value)
-   local newlist = {"resf", "uwua", "fsk"}
-   dropdown:Clear()
-   wait(1)
-   dropdown:Add(newlist)
+local dropdown = Section1:CreateDropdown("Refresh Dropdown ", {
+	List = {"Value1", "Value2", "Value3", "Value4"},
+	Default = "None"}, function(value)
+	print(value)
+end)
+
+Section1:CreateButton("Refresh Example", function ()
+	local newlist = {"resf", "uwua", "fsk"}
+	dropdown:Clear()
+	wait(1)
+	dropdown:Add(newlist)
 end)
